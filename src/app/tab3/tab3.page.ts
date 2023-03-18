88db028918b40699f7aa415fdf64d48193048c74
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2023-03-17 22:20:03 -0300

    Adicionado a funcao de rolar um dado de tamanho variavel

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,4 +9,19 @@ export class Tab3Page {
 
   constructor() {}
 
+  tamanho = 6;
+  valorAtual = 0;
+  imgsDado =[
+    'assets/dice-red.png', // Posição 0
+    'assets/dice.gif' // Posição 1
+  ]
+  imagemDado = this.imgsDado[0];
+
+  jogarDado(){
+    this.valorAtual = 0;
+    this.imagemDado = this.imgsDado[1];
+    setTimeout(() => {
+      this.valorAtual = Math.floor(Math.random() * this.tamanho) + 1;
+    }, 1000)
+  }
 }
